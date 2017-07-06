54936ed183320b66319229d708f62ffa09b695f1
Chris Cameron <chris.cameron@wedoresi.com>
2017-07-06 10:52:19 -0400

    IDmMdNXNAdapterSettings implements IDeviceSettings

--- ICD.Connect.Routing.CrestronPro/DigitalMedia/DmMdNXN/IDmMdNXNAdapterSettings.cs
+++ ICD.Connect.Routing.CrestronPro/DigitalMedia/DmMdNXN/IDmMdNXNAdapterSettings.cs
@@ -3,7 +3,7 @@ using ICD.Connect.Settings;
 namespace ICD.Connect.Routing.CrestronPro.DigitalMedia.DmMdNXN
 {
 // ReSharper disable once InconsistentNaming
-	public interface IDmMdNXNAdapterSettings : ISettings
+	public interface IDmMdNXNAdapterSettings : IDeviceSettings
 	{
 		byte Ipid { get; set; }
 	}
