e580ed180f904005015cb804b3c6f48d40035981
Chris Cameron <chris.cameron@wedoresi.com>
2017-09-20 16:16:24 -0400

    Begin implementing SwitcherCache tests

---
+++ ICD.Connect.Routing.Tests/Utils/SwitcherCacheTest.cs
@@ -0,0 +1,80 @@
+﻿using ICD.Connect.Routing.Connections;
+using ICD.Connect.Routing.EventArguments;
+using ICD.Connect.Routing.Utils;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace ICD.Connection.Routing.Tests.Utils
+{
+	[TestFixture]
+	public sealed class SwitcherCacheTest
+	{
+		[Test]
+		public void ClearTest()
+		{
+			Assert.Inconclusive();
+		}
+
+		[Test]
+		public void GetSourceDetectedStateTest()
+		{
+			SwitcherCache cache = new SwitcherCache();
+
+			Assert.IsFalse(cache.GetSourceDetectedState(1, eConnectionType.Video));
+
+			cache.SetSourceDetectedState(1, eConnectionType.Video, true);
+			Assert.IsTrue(cache.GetSourceDetectedState(1, eConnectionType.Video));
+			Assert.IsFalse(cache.GetSourceDetectedState(1, eConnectionType.Audio));
+
+			Assert.Throws<ArgumentException>(() => cache.GetSourceDetectedState(1, eConnectionType.Audio | eConnectionType.Video));
+		}
+
+		[Test]
+		public void SetSourceDetectedStateTest()
+		{
+			List<SourceDetectionStateChangeEventArgs> eventArgs = new List<SourceDetectionStateChangeEventArgs>();
+
+			SwitcherCache cache = new SwitcherCache();
+			cache.OnSourceDetectionStateChange += (s, e) => eventArgs.Add(e);
+
+			Assert.IsFalse(cache.SetSourceDetectedState(1, eConnectionType.Video, false));
+			Assert.IsTrue(cache.SetSourceDetectedState(1, eConnectionType.Video, true));
+			Assert.IsFalse(cache.SetSourceDetectedState(1, eConnectionType.Video, true));
+			Assert.IsTrue(cache.SetSourceDetectedState(1, eConnectionType.Video | eConnectionType.Audio, true));
+			Assert.IsTrue(cache.SetSourceDetectedState(1, eConnectionType.Video, false));
+
+			Assert.AreEqual(3, eventArgs.Count);
+
+			Assert.AreEqual(1, eventArgs[0].Input);
+			Assert.AreEqual(true, eventArgs[0].State);
+			Assert.AreEqual(eConnectionType.Video, eventArgs[0].Type);
+
+			Assert.AreEqual(1, eventArgs[1].Input);
+			Assert.AreEqual(true, eventArgs[1].State);
+			Assert.AreEqual(eConnectionType.Audio, eventArgs[1].Type);
+
+			Assert.AreEqual(1, eventArgs[2].Input);
+			Assert.AreEqual(false, eventArgs[2].State);
+			Assert.AreEqual(eConnectionType.Video, eventArgs[2].Type);
+		}
+
+		[Test]
+		public void SetInputForOutputTest()
+		{
+			Assert.Inconclusive();
+		}
+
+		[Test]
+		public void GetInputForOutputTest()
+		{
+			Assert.Inconclusive();
+		}
+
+		[Test]
+		public void GetInputsForOutputTest()
+		{
+			Assert.Inconclusive();
+		}
+	}
+}
