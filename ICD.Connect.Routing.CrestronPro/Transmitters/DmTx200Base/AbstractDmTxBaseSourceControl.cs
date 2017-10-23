2c96ec48b5867c96e89e849e9fb05d082ee63bc5
Chris Cameron <chris.cameron@wedoresi.com>
2017-10-23 13:47:19 -0400

    Tidying

--- ICD.Connect.Routing.CrestronPro/Transmitters/DmTx200Base/AbstractDmTxBaseSourceControl.cs
+++ ICD.Connect.Routing.CrestronPro/Transmitters/DmTx200Base/AbstractDmTxBaseSourceControl.cs
@@ -186,7 +186,7 @@ namespace ICD.Connect.Routing.CrestronPro.Transmitters.DmTx200Base
 			m_Transmitter = transmitter;
 			Subscribe(m_Transmitter);
 
-			ActiveTransmissionState = HdmiDetected || VgaDetected;
+			UpdateActiveTransmissionState();
 		}
 
 		#endregion
@@ -228,7 +228,7 @@ namespace ICD.Connect.Routing.CrestronPro.Transmitters.DmTx200Base
 		/// <param name="args"></param>
 		private void VgaInputOnInputStreamChange(EndpointInputStream inputStream, EndpointInputStreamEventArgs args)
 		{
-				ActiveTransmissionState = HdmiDetected || VgaDetected;
+			UpdateActiveTransmissionState();
 		}
 
 		/// <summary>
@@ -238,7 +238,12 @@ namespace ICD.Connect.Routing.CrestronPro.Transmitters.DmTx200Base
 		/// <param name="args"></param>
 		private void HdmiInputOnInputStreamChange(EndpointInputStream inputStream, EndpointInputStreamEventArgs args)
 		{
-				ActiveTransmissionState = HdmiDetected || VgaDetected;
+			UpdateActiveTransmissionState();
+		}
+
+		private void UpdateActiveTransmissionState()
+		{
+			ActiveTransmissionState = HdmiDetected || VgaDetected;
 		}
 
 		/// <summary>
