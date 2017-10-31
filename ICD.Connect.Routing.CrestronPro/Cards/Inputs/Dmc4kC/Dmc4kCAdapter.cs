f5030bc46b07912b3fe22651c9498d36eddfb460
Jack Kanarish <jack.kanarish@wedoresi.com>
2017-10-31 12:55:58 -0400

    Add classes for Dmc4kC and Dmc4kCHcdp2

---
+++ ICD.Connect.Routing.CrestronPro/Cards/Inputs/Dmc4kC/Dmc4kCAdapter.cs
@@ -0,0 +1,47 @@
+﻿#if SIMPLSHARP
+using Crestron.SimplSharpPro;
+using Crestron.SimplSharpPro.DM;
+#endif
+
+namespace ICD.Connect.Routing.CrestronPro.Cards.Inputs.Dmc4kC
+{
+#if SIMPLSHARP
+    public sealed class Dmc4kCAdapter : AbstractCardAdapter<Crestron.SimplSharpPro.DM.Cards.Dmc4kC, Dmc4kCAdapterSettings>
+    {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public Dmc4kCAdapter()
+        {
+            Controls.Add(new Dmc4kCAdapterRoutingControl(this, 0));
+        }
+
+        /// <summary>
+        /// Instantiates an external card.
+        /// </summary>
+        /// <param name="cresnetId"></param>
+        /// <param name="controlSystem"></param>
+        /// <returns></returns>
+        protected override Crestron.SimplSharpPro.DM.Cards.Dmc4kC InstantiateCardExternal(byte cresnetId,
+                                                                                         CrestronControlSystem controlSystem)
+        {
+            return new Crestron.SimplSharpPro.DM.Cards.Dmc4kC(cresnetId, controlSystem);
+        }
+
+        /// <summary>
+        /// Instantiates an internal card.
+        /// </summary>
+        /// <param name="cardNumber"></param>
+        /// <param name="switcher"></param>
+        /// <returns></returns>
+        protected override Crestron.SimplSharpPro.DM.Cards.Dmc4kC InstantiateCardInternal(uint cardNumber, Switch switcher)
+        {
+            return new Crestron.SimplSharpPro.DM.Cards.Dmc4kC(cardNumber, switcher);
+        }
+    }
+#else
+	public sealed class Dmc4kCAdapter : AbstractCardAdapter<Dmc4kCAdapterSettings>
+	{
+	}
+#endif
+}
