d617975490619baf03deb8986495c6ec9212d6c2
Jack Kanarish <jack.kanarish@wedoresi.com>
2017-11-06 17:02:16 -0500

    Initial commit of Crestron2Series project and DMPS2 device

---
+++ ICD.Connect.Routing.Crestron2Series/Dmps2ControlSystem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using Crestron.SimplSharp; // For Basic SIMPL# Classes
+using ICD.Connect.Devices;
+
+namespace ICD.Connect.Routing.Crestron2Series
+{
+    public sealed class Dmps2ControlSystem : AbstractDevice<Dmps2ControlSystemSettings>
+    {
+        /// <summary>
+        /// SIMPL+ can only execute the default constructor. If you have variables that require initialization, please
+        /// use an Initialize method
+        /// </summary>
+        public Dmps2ControlSystem()
+        {
+            
+        }
+
+        /// <summary>
+        /// Gets the current online status of the device.
+        /// </summary>
+        /// <returns></returns>
+        protected override bool GetIsOnlineStatus()
+        {
+            return false;
+        }
+    }
+}
