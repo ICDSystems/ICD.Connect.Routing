04403b62b959f784250958e05c58efd3008f8c66
Chris Cameron <chris.cameron@wedoresi.com>
2017-11-07 13:31:12 -0500

    Tidying

--- ICD.Connect.Routing.Crestron2Series/Devices/Endpoints/AbstractDmps300CEndpointDevice.cs
+++ ICD.Connect.Routing.Crestron2Series/Devices/Endpoints/AbstractDmps300CEndpointDevice.cs
@@ -1,7 +1,8 @@
 ﻿namespace ICD.Connect.Routing.Crestron2Series.Devices.Endpoints
 {
-    public abstract class AbstractDmps300CEndpointDevice<TSettings> : AbstractDmps300CDevice<TSettings>, IDmps300CEndpointDevice
+	public abstract class AbstractDmps300CEndpointDevice<TSettings> : AbstractDmps300CDevice<TSettings>,
+	                                                                  IDmps300CEndpointDevice
 		where TSettings : IDmps300CEndpointDeviceSettings, new()
-    {
+	{
 	}
 }
--- ICD.Connect.Routing.Crestron2Series/Devices/Endpoints/Receiver/Dmps300CReceiver.cs
+++ ICD.Connect.Routing.Crestron2Series/Devices/Endpoints/Receiver/Dmps300CReceiver.cs
@@ -1,13 +1,13 @@
 ﻿namespace ICD.Connect.Routing.Crestron2Series.Devices.Endpoints.Receiver
 {
-    public sealed class Dmps300CReceiver : AbstractDmps300CEndpointDevice<Dmps300CReceiverSettings>
-    {
+	public sealed class Dmps300CReceiver : AbstractDmps300CEndpointDevice<Dmps300CReceiverSettings>
+	{
 		/// <summary>
 		/// Constructor.
 		/// </summary>
-	    public Dmps300CReceiver()
-	    {
-		    Controls.Add(new Dmps300CReceiverDestinationControl(this));
-	    }
-    }
+		public Dmps300CReceiver()
+		{
+			Controls.Add(new Dmps300CReceiverDestinationControl(this));
+		}
+	}
 }
