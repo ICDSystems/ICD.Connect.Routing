8ce09e70e7ad22faa65f7df1b21dc7498ed1f529
Chris Cameron <chris.cameron@wedoresi.com>
2017-11-07 13:44:50 -0500

    Endpoints connecting to DMPS2

--- ICD.Connect.Routing.Crestron2Series/Devices/Endpoints/AbstractDmps300CEndpointDevice.cs
+++ ICD.Connect.Routing.Crestron2Series/Devices/Endpoints/AbstractDmps300CEndpointDevice.cs
@@ -1,8 +1,35 @@
-﻿namespace ICD.Connect.Routing.Crestron2Series.Devices.Endpoints
+﻿using ICD.Connect.Routing.Crestron2Series.Devices.ControlSystem;
+using ICD.Connect.Settings.Core;
+
+namespace ICD.Connect.Routing.Crestron2Series.Devices.Endpoints
 {
 	public abstract class AbstractDmps300CEndpointDevice<TSettings> : AbstractDmps300CDevice<TSettings>,
 	                                                                  IDmps300CEndpointDevice
 		where TSettings : IDmps300CEndpointDeviceSettings, new()
 	{
+		private Dmps300CControlSystem m_Parent;
+
+		protected override void ClearSettingsFinal()
+		{
+			base.ClearSettingsFinal();
+
+			m_Parent = null;
+			Address = null;
+		}
+
+		protected override void CopySettingsFinal(TSettings settings)
+		{
+			base.CopySettingsFinal(settings);
+
+			settings.Device = m_Parent == null ? 0 : m_Parent.Id;
+		}
+
+		protected override void ApplySettingsFinal(TSettings settings, IDeviceFactory factory)
+		{
+			base.ApplySettingsFinal(settings, factory);
+
+			m_Parent = factory.GetOriginatorById<Dmps300CControlSystem>(settings.Device);
+			Address = m_Parent.Address;
+		}
 	}
 }
--- ICD.Connect.Routing.Crestron2Series/Devices/Endpoints/Transmitter/Dmps300CTransmitter.cs
+++ ICD.Connect.Routing.Crestron2Series/Devices/Endpoints/Transmitter/Dmps300CTransmitter.cs
@@ -1,13 +1,44 @@
+using ICD.Connect.Settings.Core;
+
 namespace ICD.Connect.Routing.Crestron2Series.Devices.Endpoints.Transmitter
 {
-	public sealed class Dmps300CTransmitter : AbstractDmps300CDevice<Dmps300CTransmitterSettings>
+	public sealed class Dmps300CTransmitter : AbstractDmps300CEndpointDevice<Dmps300CTransmitterSettings>
 	{
+		private const ushort START_PORT = 8710;
+		private const ushort PORT_INCREMENT = 10;
+
+		private int m_DmInput;
+
 		/// <summary>
 		/// Constructor.
 		/// </summary>
 		public Dmps300CTransmitter()
 		{
 			Controls.Add(new Dmps300CTransmitterSourceControl(this));
+		}
+
+		protected override void ClearSettingsFinal()
+		{
+			base.ClearSettingsFinal();
+
+			m_DmInput = 0;
+		}
+
+		protected override void CopySettingsFinal(Dmps300CTransmitterSettings settings)
+		{
+			base.CopySettingsFinal(settings);
+
+			settings.DmInput = m_DmInput;
+		}
+
+		protected override void ApplySettingsFinal(Dmps300CTransmitterSettings settings, IDeviceFactory factory)
+		{
+			base.ApplySettingsFinal(settings, factory);
+
+			m_DmInput = settings.DmInput;
+			Port = (ushort)(START_PORT + PORT_INCREMENT * (m_DmInput - 1));
+
+			Connect();
 		}
 	}
 }
