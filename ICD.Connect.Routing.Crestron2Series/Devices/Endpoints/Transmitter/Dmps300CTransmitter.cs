e0af36a834acf1e254339c7ee53ba6e9fbaf1c65
Chris Cameron <chris.cameron@wedoresi.com>
2017-11-07 13:31:23 -0500

    Transmitter adds source control

--- ICD.Connect.Routing.Crestron2Series/Devices/Endpoints/Transmitter/Dmps300CTransmitter.cs
+++ ICD.Connect.Routing.Crestron2Series/Devices/Endpoints/Transmitter/Dmps300CTransmitter.cs
@@ -1,7 +1,13 @@
 namespace ICD.Connect.Routing.Crestron2Series.Devices.Endpoints.Transmitter
 {
-    public sealed class Dmps300CTransmitter : AbstractDmps300CDevice<Dmps300CTransmitterSettings>
-    {
-		
+	public sealed class Dmps300CTransmitter : AbstractDmps300CDevice<Dmps300CTransmitterSettings>
+	{
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		public Dmps300CTransmitter()
+		{
+			Controls.Add(new Dmps300CTransmitterSourceControl(this));
+		}
 	}
 }
