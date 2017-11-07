62f7b1a9d3b33d977562408c245949289b5e9e3c
Chris Cameron <chris.cameron@wedoresi.com>
2017-11-07 14:55:08 -0500

    Dmps300CReceiverDestinationControl is a midpoint

--- ICD.Connect.Routing.Crestron2Series/Devices/Endpoints/Receiver/Dmps300CReceiverDestinationControl.cs
+++ ICD.Connect.Routing.Crestron2Series/Devices/Endpoints/Receiver/Dmps300CReceiverDestinationControl.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using ICD.Common.Properties;
 using ICD.Common.Utils;
 using ICD.Common.Utils.Extensions;
 using ICD.Connect.Protocol.EventArguments;
@@ -8,18 +9,50 @@ using ICD.Connect.Protocol.XSig;
 using ICD.Connect.Routing.Connections;
 using ICD.Connect.Routing.Controls;
 using ICD.Connect.Routing.EventArguments;
-using ICD.Connect.Routing.Utils;
 
 namespace ICD.Connect.Routing.Crestron2Series.Devices.Endpoints.Receiver
 {
-    public sealed class Dmps300CReceiverDestinationControl : AbstractRouteDestinationControl<Dmps300CReceiver>
+    public sealed class Dmps300CReceiverDestinationControl : AbstractRouteMidpointControl<Dmps300CReceiver>
     {
 	    private const ushort DIGITAL_SOURCE_DETECTION_JOIN = 6;
 
 	    public override event EventHandler<SourceDetectionStateChangeEventArgs> OnSourceDetectionStateChange;
 	    public override event EventHandler<ActiveInputStateChangeEventArgs> OnActiveInputsChanged;
+	    public override event EventHandler<TransmissionStateEventArgs> OnActiveTransmissionStateChanged;
 
-	    private readonly SwitcherCache m_Cache;
+		private bool m_VideoDetected;
+
+		/// <summary>
+		/// Returns true when video is detected.
+		/// </summary>
+		[PublicAPI]
+		public bool VideoDetected
+		{
+			get { return m_VideoDetected; }
+			private set
+			{
+				if (value == m_VideoDetected)
+					return;
+
+				m_VideoDetected = value;
+
+				OnSourceDetectionStateChange.Raise(this,
+												   new SourceDetectionStateChangeEventArgs(1,
+																						   eConnectionType.Audio |
+																						   eConnectionType.Video,
+																						   m_VideoDetected));
+				OnActiveTransmissionStateChanged.Raise(this,
+													   new TransmissionStateEventArgs(1,
+																					  eConnectionType.Audio | eConnectionType.Video,
+																					  m_VideoDetected));
+			}
+		}
+
+		/// <summary>
+		/// Gets the active transmission state.
+		/// </summary>
+		[PublicAPI]
+		public bool ActiveTransmissionState { get { return VideoDetected; } }
 
 		/// <summary>
 		/// Constructor.
@@ -28,9 +61,6 @@ namespace ICD.Connect.Routing.Crestron2Series.Devices.Endpoints.Receiver
 	    public Dmps300CReceiverDestinationControl(Dmps300CReceiver parent)
 			: base(parent, 0)
 	    {
-			m_Cache = new SwitcherCache();
-
-			Subscribe(m_Cache);
 		    Subscribe(parent);
 	    }
 
@@ -42,10 +72,10 @@ namespace ICD.Connect.Routing.Crestron2Series.Devices.Endpoints.Receiver
 	    {
 		    OnSourceDetectionStateChange = null;
 		    OnActiveInputsChanged = null;
+		    OnActiveTransmissionStateChanged = null;
 
 		    base.DisposeFinal(disposing);
 
-			Unsubscribe(m_Cache);
 		    Unsubscribe(Parent);
 	    }
 
@@ -53,19 +83,54 @@ namespace ICD.Connect.Routing.Crestron2Series.Devices.Endpoints.Receiver
 
 	    public override bool GetSignalDetectedState(int input, eConnectionType type)
 	    {
-		    if (EnumUtils.HasMultipleFlags(type))
-		    {
-			    return EnumUtils.GetFlagsExceptNone(type)
-			                    .Select(f => GetSignalDetectedState(input, f))
-			                    .Unanimous(false);
-		    }
+			if (EnumUtils.HasMultipleFlags(type))
+			{
+				return EnumUtils.GetFlagsExceptNone(type)
+								.Select(f => GetSignalDetectedState(input, f))
+								.Unanimous(false);
+			}
 
-		    return m_Cache.GetSourceDetectedState(input, type);
+			if (input != 1)
+			{
+				string message = string.Format("{0} has no {1} input at address {2}", this, type, input);
+				throw new KeyNotFoundException(message);
+			}
+
+			switch (type)
+			{
+				case eConnectionType.Audio:
+					return true;
+				case eConnectionType.Video:
+					return VideoDetected;
+
+				default:
+					throw new ArgumentOutOfRangeException("type", string.Format("Unexpected value {0}", type));
+			}
+	    }
+
+	    /// <summary>
+	    /// Returns the outputs.
+	    /// </summary>
+	    /// <returns></returns>
+	    public override IEnumerable<ConnectorInfo> GetOutputs()
+	    {
+			yield return new ConnectorInfo(1, eConnectionType.Audio | eConnectionType.Video);
 	    }
 
 	    public override bool GetInputActiveState(int input, eConnectionType type)
 	    {
 		    return GetSignalDetectedState(input, type);
+	    }
+
+	    /// <summary>
+	    /// Gets the input for the given output.
+	    /// </summary>
+	    /// <param name="output"></param>
+	    /// <param name="type"></param>
+	    /// <returns></returns>
+	    public override IEnumerable<ConnectorInfo> GetInputs(int output, eConnectionType type)
+	    {
+			return GetInputs().Where(c => c.ConnectionType.HasFlags(type));
 	    }
 
 	    public override IEnumerable<ConnectorInfo> GetInputs()
@@ -115,37 +180,11 @@ namespace ICD.Connect.Routing.Crestron2Series.Devices.Endpoints.Receiver
 		    switch (data.Index)
 		    {
 			    case DIGITAL_SOURCE_DETECTION_JOIN:
-				    m_Cache.SetSourceDetectedState(1, eConnectionType.Video, data.Value);
+				    VideoDetected = data.Value;
 				    break;
 		    }
 	    }
 
 		#endregion
-
-	    #region Cache Callbacks
-
-	    private void Subscribe(SwitcherCache cache)
-	    {
-		    cache.OnSourceDetectionStateChange += CacheOnSourceDetectionStateChange;
-			cache.OnActiveInputsChanged += CacheOnActiveInputsChanged;
-	    }
-
-	    private void Unsubscribe(SwitcherCache cache)
-	    {
-			cache.OnSourceDetectionStateChange -= CacheOnSourceDetectionStateChange;
-		    cache.OnActiveInputsChanged -= CacheOnActiveInputsChanged;
-		}
-
-	    private void CacheOnActiveInputsChanged(object sender, ActiveInputStateChangeEventArgs args)
-	    {
-		    OnActiveInputsChanged.Raise(this, new ActiveInputStateChangeEventArgs(args));
-	    }
-
-	    private void CacheOnSourceDetectionStateChange(object sender, SourceDetectionStateChangeEventArgs args)
-	    {
-		    OnSourceDetectionStateChange.Raise(this, new SourceDetectionStateChangeEventArgs(args));
-	    }
-
-	    #endregion
     }
 }
