a2e86bf66ccf040d33ec3efbc7ddd5e73b14bb06
Chris Cameron <chris.cameron@wedoresi.com>
2017-11-17 15:29:59 -0500

    Moved IDmParent from misc

---
+++ ICD.Connect.Routing.CrestronPro/IDmParent.cs
@@ -0,0 +1,32 @@
+﻿#if SIMPLSHARP
+using Crestron.SimplSharpPro.DM;
+#endif
+using ICD.Common.Properties;
+
+namespace ICD.Connect.Misc.CrestronPro.Devices
+{
+	/// <summary>
+	/// A IDmParent provides methods for getting attached DM devices.
+	/// </summary>
+	[PublicAPI]
+	public interface IDmParent
+	{
+#if SIMPLSHARP
+		/// <summary>
+		/// Gets the DMInput at the given address.
+		/// </summary>
+		/// <param name="address"></param>
+		/// <returns></returns>
+		[PublicAPI]
+		DMInput GetDmInput(int address);
+
+		/// <summary>
+		/// Gets the DMOutput at the given address.
+		/// </summary>
+		/// <param name="address"></param>
+		/// <returns></returns>
+		[PublicAPI]
+		DMOutput GetDmOutput(int address);
+#endif
+	}
+}
