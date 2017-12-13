7feb9edcebe2cf86dcb72f259aebf1580dcd2bfa
Chris Cameron <chris.cameron@wedoresi.com>
2017-12-13 18:22:56 -0500

    Removing unused using directives, fixing net standard

--- ICD.Connect.Routing.Crestron2Series/Devices/IDmps300CComPortDevice.cs
+++ ICD.Connect.Routing.Crestron2Series/Devices/IDmps300CComPortDevice.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Crestron.SimplSharp;
-
-namespace ICD.Connect.Routing.Crestron2Series.Devices
+﻿namespace ICD.Connect.Routing.Crestron2Series.Devices
 {
 	public interface IDmps300CComPortDevice : IDmps300CDevice
 	{
