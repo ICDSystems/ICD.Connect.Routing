9df08d721e1a107dc2d40c63f8e4f8371dd594a2
Jack Kanarish <jack.kanarish@wedoresi.com>
2017-12-14 09:50:32 -0500

    Add Dmc4kHdo

---
+++ ICD.Connect.Routing.CrestronPro/Cards/Outputs/Dmc4kHdo/Dmc4kHdoAdapterRoutingControl.cs
@@ -0,0 +1,19 @@
+﻿#if SIMPLSHARP
+
+namespace ICD.Connect.Routing.CrestronPro.Cards.Outputs.Dmc4kHdo
+{
+// ReSharper disable once InconsistentNaming
+    public sealed class Dmc4kHdoAdapterRoutingControl : AbstractOutputCardAdapterRoutingControl<Dmc4kHdoAdapter>
+    {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="parent"></param>
+        /// <param name="id"></param>
+        public Dmc4kHdoAdapterRoutingControl(Dmc4kHdoAdapter parent, int id)
+            : base(parent, id)
+        {
+        }
+    }
+}
+#endif
