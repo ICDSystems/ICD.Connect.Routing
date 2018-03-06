3e9b73e56ff74893e7561115feb6e96aebd288c8
Chris Cameron <chris.cameron@wedoresi.com>
2018-03-06 17:25:21 -0500

    Initial commit of Atlona projects

--- ICD.Connect.Routing.Atlona/Properties/AssemblyInfo.cs
+++ ICD.Connect.Routing.Atlona/Properties/AssemblyInfo.cs
@@ -1,8 +1,8 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyTitle("ICD.Connect.Routing.Crestron2Series")]
+[assembly: AssemblyTitle("ICD.Connect.Routing.Atlona")]
 [assembly: AssemblyCompany("ICD Systems")]
-[assembly: AssemblyProduct("ICD.Connect.Routing.Crestron2Series")]
+[assembly: AssemblyProduct("ICD.Connect.Routing.Atlona")]
 [assembly: AssemblyCopyright("Copyright © ICD Systems 2018")]
 [assembly: AssemblyVersion("1.0.0.*")]
 
