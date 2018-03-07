d982ab89bde4f68572e98df1229cb75f96c4e0cd
Chris Cameron <chris.cameron@wedoresi.com>
2018-03-07 15:30:00 -0500

    Initial commit of AtUhdHdvs300DeviceSerialBuffer

---
+++ ICD.Connect.Routing.Atlona.Tests/AtUhdHdvs300DeviceSerialBufferTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using ICD.Common.Utils.EventArguments;
+using NUnit.Framework;
+
+namespace ICD.Connect.Routing.Atlona.Tests
+{
+	[TestFixture]
+	public sealed class AtUhdHdvs300DeviceSerialBufferTest
+	{
+		[Test]
+		public void CompletedStringEventTest()
+		{
+			List<StringEventArgs> completedSerials = new List<StringEventArgs>();
+			List<EventArgs> login = new List<EventArgs>();
+			List<EventArgs> password = new List<EventArgs>();
+			List<EventArgs> empty = new List<EventArgs>();
+
+			//List<StringEventArgs> receivedEvents = new List<StringEventArgs>();
+			AtUhdHdvs300DeviceSerialBuffer buffer = new AtUhdHdvs300DeviceSerialBuffer();
+
+			buffer.OnCompletedSerial += (sender, e) => completedSerials.Add(e);
+			buffer.OnLoginPrompt += (sender, e) => login.Add(e);
+			buffer.OnPasswordPrompt += (sender, e) => password.Add(e);
+			buffer.OnEmptyPrompt += (sender, e) => empty.Add(e);
+
+			buffer.Enqueue("\r\nLogin:");
+			Assert.AreEqual(1, login.Count);
+
+			buffer.Enqueue("\r\nPassword:");
+			Assert.AreEqual(1, password.Count);
+
+			buffer.Enqueue("\r\n");
+			Assert.AreEqual(1, empty.Count);
+
+			buffer.Enqueue("x4AVx1\r\n");
+			buffer.Enqueue("x1$ on\r\n");
+
+			Assert.AreEqual(1, login.Count);
+			Assert.AreEqual(1, password.Count);
+			Assert.AreEqual(1, empty.Count);
+
+			Assert.AreEqual(2, completedSerials.Count);
+			Assert.AreEqual("x4AVx1", completedSerials[0].Data);
+			Assert.AreEqual("x1$ on", completedSerials[1].Data);
+		}
+	}
+}
---
+++ ICD.Connect.Routing.Atlona/AtUhdHdvs300DeviceSerialBuffer.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ICD.Common.Utils;
+using ICD.Common.Utils.EventArguments;
+using ICD.Common.Utils.Extensions;
+using ICD.Connect.Protocol.SerialBuffers;
+
+namespace ICD.Connect.Routing.Atlona
+{
+	/// <summary>
+	/// AtUhdHdvs300DeviceSerialBuffer raises login prompts, delimits on \r\n and raises empty serials.
+	/// </summary>
+	public sealed class AtUhdHdvs300DeviceSerialBuffer : ISerialBuffer
+	{
+		public event EventHandler<StringEventArgs> OnCompletedSerial;
+		public event EventHandler OnLoginPrompt;
+		public event EventHandler OnPasswordPrompt;
+		public event EventHandler OnEmptyPrompt;
+
+		private readonly StringBuilder m_RxData;
+		private readonly Queue<string> m_Queue;
+
+		private readonly SafeCriticalSection m_QueueSection;
+		private readonly SafeCriticalSection m_ParseSection;
+
+		#region Constructors
+
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		public AtUhdHdvs300DeviceSerialBuffer()
+		{
+			m_RxData = new StringBuilder();
+			m_Queue = new Queue<string>();
+
+			m_QueueSection = new SafeCriticalSection();
+			m_ParseSection = new SafeCriticalSection();
+		}
+
+		#endregion
+
+		#region Methods
+
+		/// <summary>
+		/// Enqueues the serial data.
+		/// </summary>
+		/// <param name="data"></param>
+		public void Enqueue(string data)
+		{
+			m_QueueSection.Execute(() => m_Queue.Enqueue(data));
+			Parse();
+		}
+
+		/// <summary>
+		/// Clears all queued data in the buffer.
+		/// </summary>
+		public void Clear()
+		{
+			m_ParseSection.Enter();
+			m_QueueSection.Enter();
+
+			try
+			{
+				m_RxData.Clear();
+				m_Queue.Clear();
+			}
+			finally
+			{
+				m_ParseSection.Leave();
+				m_QueueSection.Leave();
+			}
+		}
+
+		#endregion
+
+		/// <summary>
+		/// Searches the enqueued serial data for the delimiter character.
+		/// Complete strings are raised via the OnCompletedString event.
+		/// </summary>
+		private void Parse()
+		{
+			if (!m_ParseSection.TryEnter())
+				return;
+
+			try
+			{
+				string data = null;
+
+				while (m_QueueSection.Execute(() => m_Queue.Dequeue(out data)))
+				{
+					while (true)
+					{
+						// Check for login prompt
+						if (data.StartsWith("\r\nLogin:"))
+						{
+							OnLoginPrompt.Raise(this);
+							data = data.Substring("\r\nLogin:".Length);
+						}
+
+						// Check for password prompt
+						if (data.StartsWith("\r\nPassword:"))
+						{
+							OnPasswordPrompt.Raise(this);
+							data = data.Substring("\r\nPassword:".Length);
+						}
+
+						// Check for login successful prompt
+						if (data.StartsWith("\r\n"))
+						{
+							OnEmptyPrompt.Raise(this);
+							data = data.Substring("\r\n".Length);
+						}
+
+						int index = data.IndexOf("\r\n");
+
+						if (index < 0)
+						{
+							m_RxData.Append(data);
+							break;
+						}
+
+						m_RxData.Append(data.Substring(0, index));
+						data = data.Substring(index + 2);
+
+						string output = m_RxData.Pop();
+						if (string.IsNullOrEmpty(output))
+							continue;
+
+						OnCompletedSerial.Raise(this, new StringEventArgs(output));
+					}
+				}
+			}
+			finally
+			{
+				m_ParseSection.Leave();
+			}
+		}
+	}
+}
