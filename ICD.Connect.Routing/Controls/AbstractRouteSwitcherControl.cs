5a14499986e40a51f7e786553532add3d70a0169
Chris Cameron <chris.cameron@wedoresi.com>
2018-03-30 16:42:15 -0400

    Moving routing console commands into new classes

--- ICD.Connect.Routing/Controls/AbstractRouteSwitcherControl.cs
+++ ICD.Connect.Routing/Controls/AbstractRouteSwitcherControl.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using ICD.Common.Utils;
 using ICD.Connect.API.Commands;
+using ICD.Connect.API.Nodes;
 using ICD.Connect.Devices;
 using ICD.Connect.Routing.Connections;
 using ICD.Connect.Routing.EventArguments;
@@ -40,6 +41,17 @@ namespace ICD.Connect.Routing.Controls
 		#region Console
 
 		/// <summary>
+		/// Calls the delegate for each console status item.
+		/// </summary>
+		/// <param name="addRow"></param>
+		public override void BuildConsoleStatus(AddStatusRowDelegate addRow)
+		{
+			base.BuildConsoleStatus(addRow);
+
+			RouteSwitcherControlConsole.BuildConsoleStatus(this, addRow);
+		}
+
+		/// <summary>
 		/// Gets the child console commands.
 		/// </summary>
 		/// <returns></returns>
@@ -48,15 +60,8 @@ namespace ICD.Connect.Routing.Controls
 			foreach (IConsoleCommand command in GetBaseConsoleCommands())
 				yield return command;
 
-			string connectionTypes = StringUtils.ArrayFormat(EnumUtils.GetValuesExceptNone<eConnectionType>());
-
-			string routeHelp = string.Format("Route <INPUT> <OUTPUT> <{0}>", connectionTypes);
-			yield return
-				new GenericConsoleCommand<int, int, eConnectionType>("Route", routeHelp, (i, o, t) => this.Route(i, o, t));
-
-			string clearOutputHelp = string.Format("Clear <OUTPUT> <{0}>", connectionTypes);
-			yield return
-				new GenericConsoleCommand<int, eConnectionType>("ClearOutput", clearOutputHelp, (o, t) => ClearOutput(o, t));
+			foreach (IConsoleCommand command in RouteSwitcherControlConsole.GetConsoleCommands(this))
+				yield return command;
 		}
 
 		/// <summary>
@@ -66,6 +71,28 @@ namespace ICD.Connect.Routing.Controls
 		private IEnumerable<IConsoleCommand> GetBaseConsoleCommands()
 		{
 			return base.GetConsoleCommands();
+		}
+
+		/// <summary>
+		/// Gets the child console nodes.
+		/// </summary>
+		/// <returns></returns>
+		public override IEnumerable<IConsoleNodeBase> GetConsoleNodes()
+		{
+			foreach (IConsoleNodeBase node in GetBaseConsoleNodes())
+				yield return node;
+
+			foreach (IConsoleNodeBase node in RouteSwitcherControlConsole.GetConsoleNodes(this))
+				yield return node;
+		}
+
+		/// <summary>
+		/// Workaround for "unverifiable code" warning.
+		/// </summary>
+		/// <returns></returns>
+		private IEnumerable<IConsoleNodeBase> GetBaseConsoleNodes()
+		{
+			return base.GetConsoleNodes();
 		}
 
 		#endregion
---
+++ ICD.Connect.Routing/Controls/RouteMidpointControlConsole.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ICD.Common.Utils;
+using ICD.Connect.API.Commands;
+using ICD.Connect.API.Nodes;
+using ICD.Connect.Routing.Connections;
+
+namespace ICD.Connect.Routing.Controls
+{
+	public static class RouteMidpointControlConsole
+	{
+		/// <summary>
+		/// Gets the child console nodes.
+		/// </summary>
+		/// <param name="instance"></param>
+		/// <returns></returns>
+		public static IEnumerable<IConsoleNodeBase> GetConsoleNodes(IRouteMidpointControl instance)
+		{
+			if (instance == null)
+				throw new ArgumentNullException("instance");
+
+			yield break;
+		}
+
+		/// <summary>
+		/// Calls the delegate for each console status item.
+		/// </summary>
+		/// <param name="instance"></param>
+		/// <param name="addRow"></param>
+		public static void BuildConsoleStatus(IRouteMidpointControl instance, AddStatusRowDelegate addRow)
+		{
+			if (instance == null)
+				throw new ArgumentNullException("instance");
+		}
+
+		/// <summary>
+		/// Gets the child console commands.
+		/// </summary>
+		/// <param name="instance"></param>
+		/// <returns></returns>
+		public static IEnumerable<IConsoleCommand> GetConsoleCommands(IRouteMidpointControl instance)
+		{
+			if (instance == null)
+				throw new ArgumentNullException("instance");
+
+			yield return
+				new ConsoleCommand("PrintRouting", "Prints a table of the current routing state",
+				                   () => PrintRouteStatusTable(instance));
+
+		}
+
+		private static string PrintRouteStatusTable(IRouteMidpointControl instance)
+		{
+			TableBuilder builder = new TableBuilder("Input", "Type", "Detected", "Outputs");
+
+			foreach (ConnectorInfo input in instance.GetInputs().OrderBy(c => c.Address))
+			{
+				bool first = true;
+
+				foreach (eConnectionType type in EnumUtils.GetFlagsExceptNone(input.ConnectionType))
+				{
+					string inputString = first ? input.Address.ToString() : string.Empty;
+					first = false;
+
+					string typeString = type.ToString();
+					string detectedString = instance.GetSignalDetectedState(input.Address, type) ? "True" : string.Empty;
+					string outputsString = StringUtils.ArrayFormat(instance.GetOutputs(input.Address, type));
+
+					builder.AddRow(inputString, typeString, detectedString, outputsString);
+				}
+			}
+
+			return builder.ToString();
+		}
+	}
+}
