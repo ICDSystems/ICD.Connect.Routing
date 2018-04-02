3784e4d6cb5ff3458eeff35c0f4c98f38e2c5ee7
Jack Kanarish <jack.kanarish@wedoresi.com>
2018-04-02 15:00:38 -0400

    new versioning commit

--- ICD.Connect.Routing.CrestronPro/Properties/AssemblyInfo.cs
+++ ICD.Connect.Routing.CrestronPro/Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
-﻿using System.Reflection;
+using System.Reflection;
 
 [assembly: AssemblyTitle("ICD.Connect.Routing.CrestronPro")]
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Routing.CrestronPro")]
 [assembly: AssemblyCopyright("Copyright © ICD Systems 2018")]
-[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyVersion("4.0.0.0")]
--- ICD.Connect.Routing.SPlus/Properties/AssemblyInfo.cs
+++ ICD.Connect.Routing.SPlus/Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
-﻿using System.Reflection;
+using System.Reflection;
 
 [assembly: AssemblyTitle("ICD.Connect.Routing.SPlus")]
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Routing.SPlus")]
 [assembly: AssemblyCopyright("Copyright © ICD Systems 2018")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("4.0.0.0")]
