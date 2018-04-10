71d8e52242eb8413232b74b1de73ebb164444512
Chris Cameron <chris.cameron@wedoresi.com>
2018-04-10 14:40:36 -0400

    feat: Using API event args

--- ICD.Connect.Routing.Crestron2Series/Ports/RelayPort/Dmps300CRelayPort.cs
+++ ICD.Connect.Routing.Crestron2Series/Ports/RelayPort/Dmps300CRelayPort.cs
@@ -1,5 +1,6 @@
 ﻿using ICD.Common.Utils.EventArguments;
 using ICD.Common.Utils.Services.Logging;
+using ICD.Connect.Devices.EventArguments;
 using ICD.Connect.Protocol.Ports.RelayPort;
 using ICD.Connect.Protocol.XSig;
 using ICD.Connect.Routing.Crestron2Series.Devices;
@@ -162,8 +163,8 @@ namespace ICD.Connect.Routing.Crestron2Series.Ports.RelayPort
 		/// Called when the parent online state changes.
 		/// </summary>
 		/// <param name="sender"></param>
-		/// <param name="boolEventArgs"></param>
-		private void DeviceOnIsOnlineStateChanged(object sender, BoolEventArgs boolEventArgs)
+		/// <param name="args"></param>
+		private void DeviceOnIsOnlineStateChanged(object sender, DeviceBaseOnlineStateApiEventArgs args)
 		{
 			UpdateCachedOnlineStatus();
 		}
