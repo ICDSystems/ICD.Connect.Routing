526fe19a6b6463e27a97acd1cd1065154d8eeb8b
Chris Cameron <chris.cameron@wedoresi.com>
2018-04-30 11:07:30 -0400

    feat: Added interface and abstraction for routing destinations that can have 1 active input at a time (e.g. displays, receivers)

---
+++ ICD.Connect.Routing/Controls/AbstractRouteInputSelectControl.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using ICD.Common.Utils.Extensions;
+using ICD.Connect.Devices;
+using ICD.Connect.Routing.EventArguments;
+
+namespace ICD.Connect.Routing.Controls
+{
+	public abstract class AbstractRouteInputSelectControl<TParent> : AbstractRouteDestinationControl<TParent>, IRouteInputSelectControl
+		where TParent : IDeviceBase
+	{
+		/// <summary>
+		/// Raised when the device starts/stops actively using an input, e.g. unroutes an input.
+		/// </summary>
+		public override event EventHandler<ActiveInputStateChangeEventArgs> OnActiveInputsChanged;
+
+		private int? m_ActiveInput;
+
+		/// <summary>
+		/// Gets the current active input.
+		/// </summary>
+		public int? ActiveInput
+		{
+			get { return m_ActiveInput; }
+			protected set
+			{
+				if (value == m_ActiveInput)
+					return;
+
+				if (value.HasValue && !ContainsInput(value.Value))
+					throw new InvalidOperationException("Can not set input to unavailable connector.");
+
+				int? old = m_ActiveInput;
+
+				m_ActiveInput = value;
+
+				// Stopped using the old input
+				if (old.HasValue)
+				{
+					ConnectorInfo oldInfo = GetInput(old.Value);
+					OnActiveInputsChanged.Raise(this, new ActiveInputStateChangeEventArgs(old.Value, oldInfo.ConnectionType, false));
+				}
+
+				// Started using the new input
+				if (m_ActiveInput.HasValue)
+				{
+					ConnectorInfo newInfo = GetInput(m_ActiveInput.Value);
+					OnActiveInputsChanged.Raise(this,
+					                            new ActiveInputStateChangeEventArgs(m_ActiveInput.Value, newInfo.ConnectionType, false));
+				}
+			}
+		}
+
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		/// <param name="parent"></param>
+		/// <param name="id"></param>
+		protected AbstractRouteInputSelectControl(TParent parent, int id)
+			: base(parent, id)
+		{
+		}
+
+		/// <summary>
+		/// Release resources.
+		/// </summary>
+		/// <param name="disposing"></param>
+		protected override void DisposeFinal(bool disposing)
+		{
+			OnActiveInputsChanged = null;
+
+			base.DisposeFinal(disposing);
+		}
+
+		/// <summary>
+		/// Sets the current active input.
+		/// </summary>
+		/// <param name="input"></param>
+		public abstract void SetActiveInput(int? input);
+	}
+}
---
+++ ICD.Connect.Routing/Controls/IRouteInputSelectControl.cs
@@ -0,0 +1,19 @@
+﻿namespace ICD.Connect.Routing.Controls
+{
+	/// <summary>
+	/// Describes a route destination where only one input may be active at a given time.
+	/// </summary>
+	public interface IRouteInputSelectControl : IRouteDestinationControl
+	{
+		/// <summary>
+		/// Gets the current active input.
+		/// </summary>
+		int? ActiveInput { get; }
+
+		/// <summary>
+		/// Sets the current active input.
+		/// </summary>
+		/// <param name="input"></param>
+		void SetActiveInput(int? input);
+	}
+}
