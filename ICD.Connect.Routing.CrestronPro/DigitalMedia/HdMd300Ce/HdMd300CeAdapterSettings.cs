0206d6d53560602163379e5f9b59a9a43c7e5198
Chris Cameron <chris.cameron@wedoresi.com>
2018-06-29 13:45:35 -0400

    feat: Initial commit of HdMdxxxCE adapters

---
+++ ICD.Connect.Routing.CrestronPro/DigitalMedia/HdMd300Ce/HdMd300CeAdapterSettings.cs
@@ -0,0 +1,10 @@
+﻿using ICD.Connect.Routing.CrestronPro.DigitalMedia.HdMdxxxCe;
+using ICD.Connect.Settings.Attributes;
+
+namespace ICD.Connect.Routing.CrestronPro.DigitalMedia.HdMd300Ce
+{
+	[KrangSettings("HdMd300CE", typeof(HdMd300CeAdapter))]
+	public sealed class HdMd300CeAdapterSettings : AbstractHdMdxxxCeAdapterSettings
+	{
+	}
+}
