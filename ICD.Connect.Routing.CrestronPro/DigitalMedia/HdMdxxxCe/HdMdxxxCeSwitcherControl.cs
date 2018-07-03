26e192d4c025b17b8a543bceb8836ba60b43f101
Chris Cameron <chris.cameron@wedoresi.com>
2018-07-03 16:25:46 -0400

    docs: Adding missing comments

--- ICD.Connect.Routing.CrestronPro/DigitalMedia/HdMdxxxCe/HdMdxxxCeSwitcherControl.cs
+++ ICD.Connect.Routing.CrestronPro/DigitalMedia/HdMdxxxCe/HdMdxxxCeSwitcherControl.cs
@@ -18,9 +18,24 @@ namespace ICD.Connect.Routing.CrestronPro.DigitalMedia.HdMdxxxCe
 {
 	public sealed class HdMdxxxCeSwitcherControl : AbstractRouteSwitcherControl<IHdMdxxxCeAdapter>
 	{
+		/// <summary>
+		/// Raised when the device starts/stops actively transmitting on an output.
+		/// </summary>
 		public override event EventHandler<TransmissionStateEventArgs> OnActiveTransmissionStateChanged;
+
+		/// <summary>
+		/// Raised when an input source status changes.
+		/// </summary>
 		public override event EventHandler<SourceDetectionStateChangeEventArgs> OnSourceDetectionStateChange;
+
+		/// <summary>
+		/// Raised when the device starts/stops actively using an input, e.g. unroutes an input.
+		/// </summary>
 		public override event EventHandler<ActiveInputStateChangeEventArgs> OnActiveInputsChanged;
+
+		/// <summary>
+		/// Called when a route changes.
+		/// </summary>
 		public override event EventHandler<RouteChangeEventArgs> OnRouteChange;
 
 		private readonly SwitcherCache m_Cache;
