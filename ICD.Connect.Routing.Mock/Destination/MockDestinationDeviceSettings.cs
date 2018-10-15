68af0d6e7e35c990b8c5a58823163f722bc0a46e
Chris Cameron <chris.cameron@wedoresi.com>
2018-10-15 16:00:49 -0400

    refactor: Removing redundant code

--- ICD.Connect.Routing.Mock/Destination/MockDestinationDeviceSettings.cs
+++ ICD.Connect.Routing.Mock/Destination/MockDestinationDeviceSettings.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using ICD.Connect.Devices;
+﻿using ICD.Connect.Devices;
 using ICD.Connect.Settings.Attributes;
 
 namespace ICD.Connect.Routing.Mock.Destination
@@ -7,19 +6,8 @@ namespace ICD.Connect.Routing.Mock.Destination
 	/// <summary>
 	/// Settings for the MockDestinationDevice.
 	/// </summary>
-	[KrangSettings(FACTORY_NAME)]
+	[KrangSettings("MockDestinationDevice", typeof(MockDestinationDevice))]
 	public sealed class MockDestinationDeviceSettings : AbstractDeviceSettings
 	{
-		private const string FACTORY_NAME = "MockDestinationDevice";
-
-		/// <summary>
-		/// Gets the originator factory name.
-		/// </summary>
-		public override string FactoryName { get { return FACTORY_NAME; } }
-
-		/// <summary>
-		/// Gets the type of the originator for this settings instance.
-		/// </summary>
-		public override Type OriginatorType { get { return typeof(MockDestinationDevice); } }
 	}
 }
