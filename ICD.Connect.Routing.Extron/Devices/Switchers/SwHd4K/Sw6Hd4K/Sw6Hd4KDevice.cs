75b8f87c06210e331a1613c1d9ae1a7293fff73a
Chris Cameron <chris.cameron@wedoresi.com>
2018-10-26 11:39:39 -0400

    feat: Adding switcher control to SWHD4K devices

--- ICD.Connect.Routing.Extron/Devices/Switchers/SwHd4K/Sw6Hd4K/Sw6Hd4KDevice.cs
+++ ICD.Connect.Routing.Extron/Devices/Switchers/SwHd4K/Sw6Hd4K/Sw6Hd4KDevice.cs
@@ -1,6 +1,15 @@
-﻿namespace ICD.Connect.Routing.Extron.Devices.Switchers.SwHd4K.Sw6Hd4K
+﻿using ICD.Connect.Routing.Extron.Controls;
+
+namespace ICD.Connect.Routing.Extron.Devices.Switchers.SwHd4K.Sw6Hd4K
 {
 	public sealed class Sw6Hd4KDevice : AbstractSwHd4KDevice<Sw6Hd4KSettings>
 	{
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		public Sw6Hd4KDevice()
+		{
+			Controls.Add(new ExtronSwitcherControl(this, 0, 6, 1));
+		}
 	}
 }
--- ICD.Connect.Routing.Extron/Devices/Switchers/SwHd4K/Sw8Hd4K/Sw8Hd4KDevice.cs
+++ ICD.Connect.Routing.Extron/Devices/Switchers/SwHd4K/Sw8Hd4K/Sw8Hd4KDevice.cs
@@ -1,6 +1,15 @@
-﻿namespace ICD.Connect.Routing.Extron.Devices.Switchers.SwHd4K.Sw8Hd4K
+﻿using ICD.Connect.Routing.Extron.Controls;
+
+namespace ICD.Connect.Routing.Extron.Devices.Switchers.SwHd4K.Sw8Hd4K
 {
 	public sealed class Sw8Hd4KDevice : AbstractSwHd4KDevice<Sw8Hd4KSettings>
 	{
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		public Sw8Hd4KDevice()
+		{
+			Controls.Add(new ExtronSwitcherControl(this, 0, 8, 1));
+		}
 	}
 }
