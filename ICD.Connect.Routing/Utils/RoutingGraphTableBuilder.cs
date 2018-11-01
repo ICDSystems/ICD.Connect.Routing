475a3b1990a4da90e773e22058119b855ca02551
Chris Cameron <chris.cameron@wedoresi.com>
2018-11-01 17:22:19 -0400

    perf: Removing redundant Distinct() call

--- ICD.Connect.Routing/Utils/RoutingGraphTableBuilder.cs
+++ ICD.Connect.Routing/Utils/RoutingGraphTableBuilder.cs
@@ -39,7 +39,6 @@ namespace ICD.Connect.Routing.Utils
 				                           : m_RoutingGraph.Connections
 				                                           .SelectMany(c => m_RoutingGraph.GetControls(c))
 				                                           .Except((IRouteControl)null)
-				                                           .Distinct()
 				                                           .OrderBy(d => d.Parent.Id)
 				                                           .ToArray();
 
