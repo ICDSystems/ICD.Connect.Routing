8349e78cd2ecdd44cbf8f3566e6953a47b0ae4b4
Drew Tingen <drew.tingen@icdsystems.com>
2019-01-07 10:13:35 -0500

    feat: Additional DM 8G Fiber device support

---
+++ ICD.Connect.Routing.CrestronPro/Cards/Outputs/DmcSoHd/DmcSoHdAdapterSettings.cs
@@ -0,0 +1,9 @@
+﻿using ICD.Connect.Settings.Attributes;
+
+namespace ICD.Connect.Routing.CrestronPro.Cards.Outputs.DmcSoHd
+{
+	[KrangSettings("DmcSoHd", typeof(DmcSoHdAdapter))]
+	public sealed class DmcSoHdAdapterSettings : AbstractOutputCardSettings
+	{
+	}
+}
