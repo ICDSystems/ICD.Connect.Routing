5db2b1f9057d05a0844b7a978bec6689a57e2c57
Chris Cameron <chris.cameron@wedoresi.com>
2019-02-07 15:32:58 -0500

    chore: Updating AssemblyInfo

--- ICD.Connect.Routing.Mock/Properties/AssemblyInfo.cs
+++ ICD.Connect.Routing.Mock/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Reflection;
 [assembly: AssemblyTitle("ICD.Connect.Routing.Mock")]
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Routing.Mock")]
-[assembly: AssemblyCopyright("Copyright © ICD Systems 2018")]
+[assembly: AssemblyCopyright("Copyright © ICD Systems 2019")]
 [assembly: AssemblyVersion("14.1.0.0")]
