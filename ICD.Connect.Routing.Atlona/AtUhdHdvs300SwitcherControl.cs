1c8b68cdff87bab25e1262d34bb9f91a6eca88aa
Jack Kanarish <jack.kanarish@wedoresi.com>
2019-03-27 11:51:07 -0400

    feat: add telemetry for atlona AtUhdHdvs300

--- ICD.Connect.Routing.Atlona/AtUhdHdvs300SwitcherControl.cs
+++ ICD.Connect.Routing.Atlona/AtUhdHdvs300SwitcherControl.cs
@@ -21,6 +21,15 @@ namespace ICD.Connect.Routing.Atlona
 		private const string REGEX_SOURCE_DETECT_SINGLE = @"^InputStatus(\d) (0|1)";
 		private const string REGEX_ROUTING = @"^x(\d)AVx1";
 
+	    private static Dictionary<int, string> s_InputIds = new Dictionary<int, string>
+	    {
+			{1, "HDMI IN 1"},
+			{2, "HDMI IN 2"},
+			{3, "VGA IN"},
+			{4, "DP IN 4"},
+			{5, "HDMI IN 5"}
+	    };
+
 	    /// <summary>
 	    /// Raised when an input source status changes.
 	    /// </summary>
@@ -193,7 +202,62 @@ namespace ICD.Connect.Routing.Atlona
 			return OutputOn ? m_Cache.GetInputConnectorInfoForOutput(output, type) : null;
 		}
 
-		/// <summary>Performs the given route operation.</summary>
+	    /// <summary>
+	    /// Returns switcher port objects to get details about the input ports on this switcher.
+	    /// </summary>
+	    /// <returns></returns>
+	    public override IEnumerable<InputPort> GetInputPorts()
+	    {
+			foreach (ConnectorInfo input in GetInputs())
+			{
+				bool supportsVideo = input.ConnectionType.HasFlag(eConnectionType.Video);
+				yield return new InputPort
+				{
+					Address = input.Address,
+					ConnectionType = input.ConnectionType,
+					InputId = GetInputId(input),
+					InputIdFeedbackSupported = true,
+					VideoInputSync = supportsVideo && GetVideoInputSyncState(input),
+					VideoInputSyncFeedbackSupported = supportsVideo
+				};
+			}
+	    }
+
+	    /// <summary>
+	    /// Returns switcher port objects to get details about the output ports on this switcher.
+	    /// </summary>
+	    /// <returns></returns>
+	    public override IEnumerable<OutputPort> GetOutputPorts()
+	    {
+			foreach (ConnectorInfo output in GetOutputs())
+			{
+				bool supportsVideo = output.ConnectionType.HasFlag(eConnectionType.Video);
+				bool supportsAudio = output.ConnectionType.HasFlag(eConnectionType.Audio);
+				yield return new OutputPort
+				{
+					Address = output.Address,
+					ConnectionType = output.ConnectionType,
+					OutputId = "HDMI OUT",
+					OutputIdFeedbackSupport = true,
+					VideoOutputSource = supportsVideo ? GetActiveSourceIdName(output, eConnectionType.Video) : null,
+					VideoOutputSourceFeedbackSupport = supportsVideo,
+					AudioOutputSource = supportsAudio ? GetActiveSourceIdName(output, eConnectionType.Audio) : null,
+					AudioOutputSourceFeedbackSupport = supportsAudio
+				};
+			}
+	    }
+
+	    private string GetInputId(ConnectorInfo info)
+	    {
+		    return ContainsInput(info.Address) ? s_InputIds[info.Address] : null;
+	    }
+
+	    private bool GetVideoInputSyncState(ConnectorInfo info)
+	    {
+		    return m_Cache.GetSourceDetectedState(info.Address, eConnectionType.Video);
+	    }
+
+	    /// <summary>Performs the given route operation.</summary>
 		/// <param name="info"></param>
 		/// <returns></returns>
 		public override bool Route(RouteOperation info)
