4280dbc95527e52af545e62bd356932be0fb17c3
Chris Cameron <chris.cameron@wedoresi.com>
2019-06-25 13:14:56 -0400

    refactor: Removing unused using directives

--- ICD.Connect.Routing.Mock/Switcher/MockRouteSwitcherControl.cs
+++ ICD.Connect.Routing.Mock/Switcher/MockRouteSwitcherControl.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using ICD.Common.Properties;
-using ICD.Common.Utils;
 using ICD.Common.Utils.Extensions;
 using ICD.Common.Utils.Services;
 using ICD.Connect.API.Commands;
--- ICD.Connect.Routing/Controls/RouteSwitcherControl.cs
+++ ICD.Connect.Routing/Controls/RouteSwitcherControl.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using ICD.Common.Utils.Extensions;
 using ICD.Connect.Routing.Connections;
 using ICD.Connect.Routing.Devices;
