5bbb5edf8877945eda7fedecfc39836e20b129c9
Austin Noska <austin.noska@profound-tech.com>
2019-07-17 15:29:44 -0400

    feat: Backported Dm-Tx-4kz-202C from ConnectPro_v1.3 for ConnectPro_v1.1 compatability

---
+++ ICD.Connect.Routing.CrestronPro/Transmitters/DmTx4kz202C/DmTx4kz202CAdapter.cs
@@ -0,0 +1,37 @@
+﻿using ICD.Connect.Routing.CrestronPro.Transmitters.DmTx4KzX02CBase;
+#if SIMPLSHARP
+using Crestron.SimplSharpPro;
+using Crestron.SimplSharpPro.DM;
+using Crestron.SimplSharpPro.DM.Endpoints.Transmitters;
+#endif
+
+namespace ICD.Connect.Routing.CrestronPro.Transmitters.DmTx4Kz202C
+{
+#if SIMPLSHARP
+	public sealed class DmTx4Kz202CAdapter : AbstractDmTx4kzX02CBaseAdapter<DmTx4kz202C, DmTx4Kz202CAdapterSettings>
+#else
+	public sealed class DmTx4Kz202CAdapter : AbstractDmTx4kzX02CBaseAdapter<DmTx4Kz202CAdapterSettings>
+#endif
+	{
+		#region Settings
+
+#if SIMPLSHARP
+		public override DmTx4kz202C InstantiateTransmitter(byte ipid, CrestronControlSystem controlSystem)
+		{
+			return new DmTx4kz202C(ipid, controlSystem);
+		}
+
+		public override DmTx4kz202C InstantiateTransmitter(byte ipid, DMInput input)
+		{
+			return new DmTx4kz202C(ipid, input);
+		}
+
+		public override DmTx4kz202C InstantiateTransmitter(DMInput input)
+		{
+			return new DmTx4kz202C(input);
+		}
+#endif
+
+		#endregion
+	}
+}
---
+++ ICD.Connect.Routing.CrestronPro/Transmitters/DmTx4kz202C/DmTx4kz202CAdapterSettings.cs
@@ -0,0 +1,10 @@
+﻿using ICD.Connect.Routing.CrestronPro.Transmitters.DmTx4KzX02CBase;
+using ICD.Connect.Settings.Attributes;
+
+namespace ICD.Connect.Routing.CrestronPro.Transmitters.DmTx4Kz202C
+{
+	[KrangSettings("DmTx4kz202C", typeof(DmTx4Kz202CAdapter))]
+	public sealed class DmTx4Kz202CAdapterSettings : AbstractDmTx4kzX02CBaseAdapterSettings
+	{
+	}
+}
