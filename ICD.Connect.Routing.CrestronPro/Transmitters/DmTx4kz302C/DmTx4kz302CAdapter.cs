fb2f05f98297604d6a9f51fb2f45298bd29ff44c
Austin Noska <austin.noska@profound-tech.com>
2019-07-17 15:29:57 -0400

    feat: Backported Dm-Tx-4kz-302C from ConnectPro_v1.3 for ConnectPro_v1.1 compatability

---
+++ ICD.Connect.Routing.CrestronPro/Transmitters/DmTx4kz302C/DmTx4kz302CAdapter.cs
@@ -0,0 +1,36 @@
+﻿#if SIMPLSHARP
+using Crestron.SimplSharpPro;
+using Crestron.SimplSharpPro.DM;
+#endif
+using ICD.Connect.Routing.CrestronPro.Transmitters.DmTx4KzX02CBase;
+
+namespace ICD.Connect.Routing.CrestronPro.Transmitters.DmTx4kz302C
+{
+#if SIMPLSHARP
+	public sealed class DmTx4Kz302CAdapter : AbstractDmTx4kzX02CBaseAdapter<Crestron.SimplSharpPro.DM.Endpoints.Transmitters.DmTx4kz302C, DmTx4Kz302CAdapterSettings>
+#else
+	public sealed class DmTx4Kz302CAdapter : AbstractDmTx4kzX02CBaseAdapter<DmTx4Kz302CAdapterSettings>
+#endif
+	{
+		#region Settings
+
+#if SIMPLSHARP
+		public override Crestron.SimplSharpPro.DM.Endpoints.Transmitters.DmTx4kz302C InstantiateTransmitter(byte ipid, CrestronControlSystem controlSystem)
+		{
+			return new Crestron.SimplSharpPro.DM.Endpoints.Transmitters.DmTx4kz302C(ipid, controlSystem);
+		}
+
+		public override Crestron.SimplSharpPro.DM.Endpoints.Transmitters.DmTx4kz302C InstantiateTransmitter(byte ipid, DMInput input)
+		{
+			return new Crestron.SimplSharpPro.DM.Endpoints.Transmitters.DmTx4kz302C(ipid, input);
+		}
+
+		public override Crestron.SimplSharpPro.DM.Endpoints.Transmitters.DmTx4kz302C InstantiateTransmitter(DMInput input)
+		{
+			return new Crestron.SimplSharpPro.DM.Endpoints.Transmitters.DmTx4kz302C(input);
+		}
+#endif
+
+		#endregion
+	}
+}
---
+++ ICD.Connect.Routing.CrestronPro/Transmitters/DmTx4kz302C/DmTx4kz302CAdapterSettings.cs
@@ -0,0 +1,10 @@
+﻿using ICD.Connect.Routing.CrestronPro.Transmitters.DmTx4KzX02CBase;
+using ICD.Connect.Settings.Attributes;
+
+namespace ICD.Connect.Routing.CrestronPro.Transmitters.DmTx4kz302C
+{
+	[KrangSettings("DmTx4kz302C", typeof(DmTx4Kz302CAdapter))]
+	public sealed class DmTx4Kz302CAdapterSettings : AbstractDmTx4kzX02CBaseAdapterSettings
+	{
+	}
+}
