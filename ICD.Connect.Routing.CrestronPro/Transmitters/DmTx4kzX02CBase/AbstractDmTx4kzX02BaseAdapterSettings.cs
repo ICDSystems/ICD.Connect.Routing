edb08f681e502977f5a5caeb42d5b4695fa558d9
Austin Noska <austin.noska@profound-tech.com>
2019-07-17 15:27:47 -0400

    feat: Backporting Dm-Tx-4kz-X02 Abstracts and Interfaces from ConnectPro_v1.3 for compatability with ConnectPro_v1.1

---
+++ ICD.Connect.Routing.CrestronPro/Transmitters/DmTx4kzX02CBase/AbstractDmTx4kzX02BaseAdapterSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace ICD.Connect.Routing.CrestronPro.Transmitters.DmTx4KzX02CBase
+{
+	public abstract class AbstractDmTx4kzX02CBaseAdapterSettings : AbstractEndpointTransmitterBaseAdapterSettings,
+																   IDmTx4kzX02CBaseAdapterSettings
+	{
+	}
+}
---
+++ ICD.Connect.Routing.CrestronPro/Transmitters/DmTx4kzX02CBase/IDmTx4kzX02CBaseAdapter.cs
@@ -0,0 +1,6 @@
+﻿namespace ICD.Connect.Routing.CrestronPro.Transmitters.DmTx4KzX02CBase
+{
+	public interface IDmTx4kzX02CBaseAdapter : IEndpointTransmitterBaseAdapter
+	{
+	}
+}
