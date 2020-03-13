f4d79edcd1d2f62578f98af98c80064c29515d54
Austin Noska <austin.noska@profound-tech.com>
2020-03-13 18:41:22 -0400

    feat: StreamSourceDevice & StreamSourceDeviceSettings

---
+++ ICD.Connect.Routing/Devices/Streaming/StreamSourceDevice.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using ICD.Common.Properties;
+using ICD.Common.Utils.EventArguments;
+using ICD.Common.Utils.Extensions;
+using ICD.Common.Utils.Services.Logging;
+using ICD.Connect.API.Commands;
+using ICD.Connect.API.Nodes;
+using ICD.Connect.Devices;
+using ICD.Connect.Settings;
+
+namespace ICD.Connect.Routing.Devices.Streaming
+{
+    public sealed class StreamSourceDevice : AbstractDevice<StreamSourceDeviceSettings>
+    {
+	    public event EventHandler<GenericEventArgs<Uri>> OnStreamUriChanged; 
+
+		[CanBeNull]
+	    private Uri m_StreamUri;
+
+		[CanBeNull]
+	    public Uri StreamUri
+	    {
+		    get { return m_StreamUri; }
+		    set
+		    {
+				if (m_StreamUri == value)
+					return;
+
+				m_StreamUri = value;
+				Log(eSeverity.Informational, "Stream Uri changed to - {0}", m_StreamUri);
+				OnStreamUriChanged.Raise(this, new GenericEventArgs<Uri>(m_StreamUri));
+		    }
+	    }
+
+
+	    protected override bool GetIsOnlineStatus()
+	    {
+		    return true;
+	    }
+
+	    #region Settings
+
+	    protected override void ApplySettingsFinal(StreamSourceDeviceSettings settings, IDeviceFactory factory)
+	    {
+		    base.ApplySettingsFinal(settings, factory);
+		    try
+		    {
+			    StreamUri = new Uri(settings.StreamUri);
+		    }
+		    catch (Exception e)
+		    {
+				Log(eSeverity.Error, "Failed to parse Stream Uri - {0}", e.Message);
+				StreamUri = null;
+		    }
+	    }
+
+	    protected override void CopySettingsFinal(StreamSourceDeviceSettings settings)
+	    {
+		    base.CopySettingsFinal(settings);
+
+		    settings.StreamUri = StreamUri != null ? StreamUri.ToString() : null;
+	    }
+
+	    protected override void ClearSettingsFinal()
+	    {
+		    base.ClearSettingsFinal();
+
+		    StreamUri = null;
+	    }
+
+	    #endregion
+
+	    public override void BuildConsoleStatus(AddStatusRowDelegate addRow)
+	    {
+		    base.BuildConsoleStatus(addRow);
+
+		    addRow("StreamUri", StreamUri);
+	    }
+
+	    public override IEnumerable<IConsoleCommand> GetConsoleCommands()
+	    {
+		    foreach (IConsoleCommand baseConsoleCommand in GetBaseConsoleCommands())
+			    yield return baseConsoleCommand;
+
+		    yield return new GenericConsoleCommand<string>("SetStreamUri", "Changes the stream uri",
+		                                                   s => StreamUri = new Uri(s));
+	    }
+
+	    private IEnumerable<IConsoleCommand> GetBaseConsoleCommands()
+	    {
+		    return base.GetConsoleCommands();
+	    }
+    }
+}
---
+++ ICD.Connect.Routing/Devices/Streaming/StreamSourceDeviceSettings.cs
@@ -0,0 +1,28 @@
+﻿using ICD.Common.Utils.Xml;
+using ICD.Connect.Devices;
+using ICD.Connect.Settings.Attributes;
+
+namespace ICD.Connect.Routing.Devices.Streaming
+{
+	[KrangSettings("StreamSourceDevice", typeof(StreamSourceDevice))]
+	public sealed class StreamSourceDeviceSettings : AbstractDeviceSettings
+	{
+		private const string STREAM_URI_ELEMENT = "StreamUri";
+
+		public string StreamUri { get; set; }
+
+		public override void ParseXml(string xml)
+		{
+			base.ParseXml(xml);
+
+			StreamUri = XmlUtils.TryReadChildElementContentAsString(xml, STREAM_URI_ELEMENT);
+		}
+
+		protected override void WriteElements(IcdXmlTextWriter writer)
+		{
+			base.WriteElements(writer);
+
+			writer.WriteElementString(STREAM_URI_ELEMENT, StreamUri);
+		}
+	}
+}
