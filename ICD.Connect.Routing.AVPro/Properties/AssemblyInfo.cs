f51872db528e46d2f54e3dcaa546b85a80598298
Chris Cameron <chris.cameron@icdsystems.com>
2020-04-21 21:07:15 -0400

    feat: Initial commit of Routing.AVPro projects

---
+++ ICD.Connect.Routing.AVPro/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+using System.Reflection;
+
+[assembly: AssemblyTitle("ICD.Connect.Routing.AVPro")]
+[assembly: AssemblyCompany("ICD Systems")]
+[assembly: AssemblyProduct("ICD.Connect.Routing.AVPro")]
+[assembly: AssemblyCopyright("Copyright © ICD Systems 2020")]
+[assembly: AssemblyVersion("15.3.0.0")]
