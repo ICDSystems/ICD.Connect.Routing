beca78893400a1b04bf15d5540312c805074cc81
Chris Cameron <chris.cameron@icdsystems.com>
2020-05-22 23:39:06 -0400

    refactor: Using AbstractSerialBuffer

--- ICD.Connect.Routing.Atlona/AtUhdHdvs300DeviceSerialBuffer.cs
+++ ICD.Connect.Routing.Atlona/AtUhdHdvs300DeviceSerialBuffer.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using ICD.Common.Utils;
-using ICD.Common.Utils.EventArguments;
 using ICD.Common.Utils.Extensions;
 using ICD.Connect.Protocol.SerialBuffers;
 
@@ -11,18 +9,13 @@ namespace ICD.Connect.Routing.Atlona
 	/// <summary>
 	/// AtUhdHdvs300DeviceSerialBuffer raises login prompts, delimits on \r\n and raises empty serials.
 	/// </summary>
-	public sealed class AtUhdHdvs300DeviceSerialBuffer : ISerialBuffer
+	public sealed class AtUhdHdvs300DeviceSerialBuffer : AbstractSerialBuffer
 	{
-		public event EventHandler<StringEventArgs> OnCompletedSerial;
 		public event EventHandler OnLoginPrompt;
 		public event EventHandler OnPasswordPrompt;
 		public event EventHandler OnEmptyPrompt;
 
 		private readonly StringBuilder m_RxData;
-		private readonly Queue<string> m_Queue;
-
-		private readonly SafeCriticalSection m_QueueSection;
-		private readonly SafeCriticalSection m_ParseSection;
 
 		#region Constructors
 
@@ -32,89 +25,44 @@ namespace ICD.Connect.Routing.Atlona
 		public AtUhdHdvs300DeviceSerialBuffer()
 		{
 			m_RxData = new StringBuilder();
-			m_Queue = new Queue<string>();
-
-			m_QueueSection = new SafeCriticalSection();
-			m_ParseSection = new SafeCriticalSection();
 		}
 
 		#endregion
 
-		#region Methods
+		#region Private Methods
 
 		/// <summary>
-		/// Enqueues the serial data.
+		/// Override to clear any current state.
+		/// </summary>
+		protected override void ClearFinal()
+		{
+			m_RxData.Clear();
+		}
+
+		/// <summary>
+		/// Override to process the given item for chunking.
 		/// </summary>
 		/// <param name="data"></param>
-		public void Enqueue(string data)
+		protected override IEnumerable<string> Process(string data)
 		{
-			m_QueueSection.Execute(() => m_Queue.Enqueue(data));
-			Parse();
-		}
-
-		/// <summary>
-		/// Clears all queued data in the buffer.
-		/// </summary>
-		public void Clear()
-		{
-			m_ParseSection.Enter();
-			m_QueueSection.Enter();
-
-			try
+			while (true)
 			{
-				m_RxData.Clear();
-				m_Queue.Clear();
-			}
-			finally
-			{
-				m_ParseSection.Leave();
-				m_QueueSection.Leave();
-			}
-		}
+				data = ParseLoginPrompts(data);
 
-		#endregion
+				int index = data.IndexOf("\r\n", StringComparison.Ordinal);
 
-		/// <summary>
-		/// Searches the enqueued serial data for the delimiter character.
-		/// Complete strings are raised via the OnCompletedString event.
-		/// </summary>
-		private void Parse()
-		{
-			if (!m_ParseSection.TryEnter())
-				return;
-
-			try
-			{
-				string data = null;
-
-				while (m_QueueSection.Execute(() => m_Queue.Dequeue(out data)))
+				if (index < 0)
 				{
-					while (true)
-					{
-						data = ParseLoginPrompts(data);
-
-						int index = data.IndexOf("\r\n", StringComparison.Ordinal);
-
-						if (index < 0)
-						{
-							m_RxData.Append(data);
-							break;
-						}
-
-						m_RxData.Append(data.Substring(0, index));
-						data = data.Substring(index + 2);
-
-						string output = m_RxData.Pop();
-						if (string.IsNullOrEmpty(output))
-							continue;
-
-						OnCompletedSerial.Raise(this, new StringEventArgs(output));
-					}
+					m_RxData.Append(data);
+					break;
 				}
-			}
-			finally
-			{
-				m_ParseSection.Leave();
+
+				m_RxData.Append(data.Substring(0, index));
+				data = data.Substring(index + 2);
+
+				string output = m_RxData.Pop();
+				if (!string.IsNullOrEmpty(output))
+					yield return output;
 			}
 		}
 
@@ -143,5 +91,7 @@ namespace ICD.Connect.Routing.Atlona
 
 			return data;
 		}
+
+		#endregion
 	}
 }
