a39f0d59c7e074543a5c10c93b79a0ed743fe5f1
Chris Cameron <chris.cameron@wedoresi.com>
2020-07-02 10:06:31 -0400

    refactor: Removing redundant interface

--- ICD.Connect.Routing.CrestronPro/ControlSystem/ControlSystemExternalTelemetryNames.cs
+++ ICD.Connect.Routing.CrestronPro/ControlSystem/ControlSystemExternalTelemetryNames.cs
@@ -2,15 +2,11 @@
 {
 	public static class ControlSystemExternalTelemetryNames
 	{
-
-
 		public const string PROGRAM_UPTIME = "Program Uptime";
 		public const string PROGRAM_UPTIME_CHANGED = "Program Uptime Changed";
 
 		public const string PROGRAMMER_NAME = "Programmer Name";
-
 		public const string SYSTEM_NAME = "System Name";
-		public const string SYSTEM_NAME_CHANGED = "System Name Changed";
 
 		public const string PROGRAM_SOURCE_FILE = "Program Source File";
 		public const string PROGRAM_COMPLIE_DATE = "Program Compile Date";
--- ICD.Connect.Routing.CrestronPro/ControlSystem/ControlSystemExternalTelemetryProvider.cs
+++ ICD.Connect.Routing.CrestronPro/ControlSystem/ControlSystemExternalTelemetryProvider.cs
@@ -5,46 +5,92 @@ using ICD.Common.Utils;
 using ICD.Common.Utils.EventArguments;
 using ICD.Common.Utils.Extensions;
 using ICD.Common.Utils.Timers;
+using ICD.Connect.Devices;
+using ICD.Connect.Telemetry.Attributes;
 using ICD.Connect.Telemetry.Providers.External;
 
 namespace ICD.Connect.Routing.CrestronPro.ControlSystem
 {
 	[UsedImplicitly]
-	public sealed class ControlSystemExternalTelemetryProvider : AbstractExternalTelemetryProvider<IControlSystemDevice>, IControlSystemExternalTelemetryProvider
+	public sealed class ControlSystemExternalTelemetryProvider : AbstractExternalTelemetryProvider<IControlSystemDevice>
 	{
 		private const string PROGRAMMER_NAME = "ICD Systems";
 		private const string SYSTEM_NAME = "ICD.Connect";
 
 		private const long UPTIME_UPDATE_TIMER_INTERVAL = 10 * 60 * 1000;
 
+		#region Events
+
+		[EventTelemetry(DeviceTelemetryNames.DEVICE_DHCP_STATUS_CHANGED)]
 		public event EventHandler<BoolEventArgs> OnDhcpStatusChanged;
+
+		[EventTelemetry(DeviceTelemetryNames.DEVICE_IP_ADDRESS_CHANGED)]
 		public event EventHandler<StringEventArgs> OnProcessorIpAddressChanged;
+
+		[EventTelemetry(DeviceTelemetryNames.DEVICE_IP_ADDRESS_SECONDARY_CHANGED)]
 		public event EventHandler<StringEventArgs> OnProcessorIpAddressSecondaryChanged;
+
+		[EventTelemetry(DeviceTelemetryNames.DEVICE_HOSTNAME_CHANGED)]
 		public event EventHandler<StringEventArgs> OnProcessorHostnameChanged;
+
+		[EventTelemetry(DeviceTelemetryNames.DEVICE_HOSTNAME_SECONDARY_CHANGED)]
 		public event EventHandler<StringEventArgs> OnProcessorHostnameSecondaryChanged;
+
+		[EventTelemetry(DeviceTelemetryNames.DEVICE_UPTIME_CHANGED)]
 		public event EventHandler<GenericEventArgs<TimeSpan>> OnProcessorUptimeChanged;
+
+		[EventTelemetry(ControlSystemExternalTelemetryNames.PROGRAM_UPTIME_CHANGED)]
 		public event EventHandler<GenericEventArgs<TimeSpan>> OnProgramUptimeChanged;
-		public event EventHandler<StringEventArgs> OnSystemNameChanged;
+
+		#endregion
 
 		private readonly IcdTimer m_UptimeUpdateTimer;
 
+		#region Properties
+
+		[PropertyTelemetry(DeviceTelemetryNames.DEVICE_DHCP_STATUS, null, DeviceTelemetryNames.DEVICE_DHCP_STATUS_CHANGED)]
 		public bool DhcpStatus { get { return IcdEnvironment.DhcpStatus; } }
 
+		[PropertyTelemetry(DeviceTelemetryNames.DEVICE_FIRMWARE_VERSION, null, null)]
 		public Version ProcessorFirmwareVersion { get { return ProcessorUtils.ModelVersion; } }
+
+		[PropertyTelemetry(DeviceTelemetryNames.DEVICE_FIRMWARE_DATE, null, null)]
 		public DateTime ProcessorFirmwareDate { get { return ProcessorUtils.ModelVersionDate; } }
+
+		[PropertyTelemetry(DeviceTelemetryNames.DEVICE_MAC_ADDRESS, null, null)]
 		public string ProcessorMacAddress { get { return IcdEnvironment.MacAddresses.FirstOrDefault() ?? string.Empty; } }
+
+		[PropertyTelemetry(DeviceTelemetryNames.DEVICE_IP_ADDRESS, null, DeviceTelemetryNames.DEVICE_IP_ADDRESS_CHANGED)]
 		public string ProcessorIpAddress { get { return IcdEnvironment.NetworkAddresses.FirstOrDefault() ?? string.Empty; } }
+
+		[PropertyTelemetry(DeviceTelemetryNames.DEVICE_IP_ADDRESS_SECONDARY, null, DeviceTelemetryNames.DEVICE_IP_ADDRESS_SECONDARY_CHANGED)]
 		public string ProcessorIpAddressSecondary { get { return IcdEnvironment.NetworkAddresses.Skip(1).FirstOrDefault() ?? string.Empty; } }
+
+		[PropertyTelemetry(DeviceTelemetryNames.DEVICE_HOSTNAME, null, DeviceTelemetryNames.DEVICE_HOSTNAME_CHANGED)]
 		public string ProcessorHostname { get { return IcdEnvironment.Hostnames.FirstOrDefault() ?? string.Empty; } }
+
+		[PropertyTelemetry(DeviceTelemetryNames.DEVICE_HOSTNAME_SECONDARY, null, DeviceTelemetryNames.DEVICE_HOSTNAME_SECONDARY_CHANGED)]
 		public string ProcessorHostnameSecondary { get { return IcdEnvironment.Hostnames.Skip(1).FirstOrDefault() ?? string.Empty; } }
 
+		[PropertyTelemetry(DeviceTelemetryNames.DEVICE_UPTIME, null, DeviceTelemetryNames.DEVICE_UPTIME_CHANGED)]
 		public TimeSpan ProcessorUptime { get { return ProcessorUtils.GetSystemUptime(); } }
+
+		[PropertyTelemetry(ControlSystemExternalTelemetryNames.PROGRAM_UPTIME, null, ControlSystemExternalTelemetryNames.PROGRAM_UPTIME_CHANGED)]
 		public TimeSpan ProgramUptime { get { return ProcessorUtils.GetProgramUptime(); } }
 
+		[PropertyTelemetry(ControlSystemExternalTelemetryNames.PROGRAMMER_NAME, null, null)]
 		public string ProgrammerName { get { return PROGRAMMER_NAME; } }
+
+		[PropertyTelemetry(ControlSystemExternalTelemetryNames.SYSTEM_NAME, null, null)]
 		public string SystemName { get { return SYSTEM_NAME; } }
+
+		[PropertyTelemetry(ControlSystemExternalTelemetryNames.PROGRAM_SOURCE_FILE, null, null)]
 		public string ProgramSourceFile { get { return ProgramUtils.ProgramFile; } }
+
+		[PropertyTelemetry(ControlSystemExternalTelemetryNames.PROGRAM_COMPLIE_DATE, null, null)]
 		public DateTime ProgramCompileDate { get { return ProgramUtils.CompiledDate; } }
+
+		#endregion
 
 		public ControlSystemExternalTelemetryProvider()
 		{
@@ -55,6 +101,8 @@ namespace ICD.Connect.Routing.CrestronPro.ControlSystem
 			m_UptimeUpdateTimer.OnElapsed += UptimeUpdateTimerOnElapsed;
 			m_UptimeUpdateTimer.Restart(UPTIME_UPDATE_TIMER_INTERVAL);
 		}
+
+		#region Private Methods
 
 		private void UptimeUpdateTimerOnElapsed(object sender, EventArgs eventArgs)
 		{
@@ -71,5 +119,7 @@ namespace ICD.Connect.Routing.CrestronPro.ControlSystem
 			OnProcessorHostnameChanged.Raise(this, new StringEventArgs(ProcessorHostname));
 			OnProcessorHostnameSecondaryChanged.Raise(this, new StringEventArgs(ProcessorHostnameSecondary));
 		}
+
+		#endregion
 	}
 }
